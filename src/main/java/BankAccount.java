923f7a562d2ffea107c595129430c0fb1f0558a8
milanboga <23mboga@athenian.org>
2021-10-27 10:34:26 -0700

    In class

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -3,4 +3,43 @@ public class BankAccount {
     private int balance;
     private int accountNumber;
     private String preferredName;
+
+    public BankAccount(int money, int accnum, String name) {
+        balance = money;
+        accountNumber = accnum;
+        preferredName = name;
+    }
+
+    public void withdraw(int a) {
+        if(balance - a >= 0) {
+            balance = balance - a;
+        }
+        else{
+            System.out.println("Nice try broke boy, no can do");
+        }
+    }
+
+    public void deposit(int a) {
+        balance = balance + a;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+
+    public String getPreferredName(){
+        return preferredName;
+    }
+
+    public void setPreferredName(String name){
+        preferredName = name;
+    }
+
+    public String toString(){
+        return "Hello " + preferredName + " your balance is " + balance + " , congrats!";
+    }
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class BankClient {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What is your preferred name?");
-        String name = scan.nextLine();
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("What is your preferred name?");
+//        String name = scan.nextLine();
 //        // Create a bank account with a balance of 0, the account number 101134, and the given name
 //        BankAccount account = new BankAccount(0, 101134, name);
+        BankAccount account = new BankAccount(10000, 4749, "Silias");
+        account.deposit(700);
+        System.out.println(account.getBalance());
+        System.out.println(account.getPreferredName());
+        account.setPreferredName("Rochell");
+        System.out.println(account.getPreferredName());
+        account.toString();
     }
 }
